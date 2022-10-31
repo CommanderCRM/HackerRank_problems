b5e5188aef6b137a76148a0a11cc37aa8f4763c7
Ilya <htctomsk634@gmail.com>
2022-10-31 18:22:07 +0700

    Update WriteAFunction.py
    indentation fix

--- Python/WriteAFunction.py
+++ Python/WriteAFunction.py
@@ -3,13 +3,13 @@ def is_leap(year):
     
     # Write your logic here
     if year % 4 == 0:
-            if year % 100 == 0:
-                if year % 400 == 0:
-                    leap = True
-                else:
-                    leap = False
-            else:
+        if year % 100 == 0:
+            if year % 400 == 0:
                 leap = True
+            else:
+                leap = False
+        else:
+            leap = True
     else:
         leap = False
     return leap
