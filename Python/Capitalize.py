711fd72e3c0b2a81cf964dc7f6e2779d690555e2
Ilya <htctomsk634@gmail.com>
2022-10-31 18:05:16 +0700

    Create Capitalize.py

---
+++ Python/Capitalize.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    splitted = s.split(" ")
+    capitalized = [name.capitalize() for name in splitted]
+    res = ' '.join([str(i) for i in capitalized])
+    return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
