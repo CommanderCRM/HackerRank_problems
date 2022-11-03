5c9e898cea7cbe970f77618cd64a339bddb7847d
Ilya <htctomsk634@gmail.com>
2022-11-03 14:32:59 +0700

    Create DesignerPDFViewer.py

---
+++ Algorithms/DesignerPDFViewer.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import string
+
+#
+# Complete the 'designerPdfViewer' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY h
+#  2. STRING word
+#
+
+def designerPdfViewer(h, word):
+    # Write your code here
+    len_word = len(word)
+    list_word = list(word)
+    dict1 = dict(zip(list(string.ascii_lowercase), h))
+    for i in range(len_word):
+        list_word[i] = dict1[list_word[i]]
+    max_value = max(list_word)
+    area = max_value * len_word
+    return area
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    h = list(map(int, input().rstrip().split()))
+
+    word = input()
+
+    result = designerPdfViewer(h, word)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
