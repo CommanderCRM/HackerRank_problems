08b55872a651bdf27c2c27a371dda5492da3ae9c
Ilya <htctomsk634@gmail.com>
2022-11-09 15:48:31 +0700

    Update ListComprehensions.py
    Fix to take n into account

--- Python/ListComprehensions.py
+++ Python/ListComprehensions.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
     y_list = list(range(y+1))
     z_list = list(range(z+1))
     perm_list = [[x,y,z] for x in x_list for y in y_list for z in z_list]
-    list_without_sums = [list for list in perm_list if sum(list) != 2]
+    list_without_sums = [list for list in perm_list if sum(list) != n]
     print(list_without_sums)
