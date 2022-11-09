e111597dc1ae83d3a1e8a0355d12c9638a01a359
Ilya <htctomsk634@gmail.com>
2022-11-09 15:46:41 +0700

    Create ListComprehensions.py

---
+++ Python/ListComprehensions.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    x_list = list(range(x+1))
+    y_list = list(range(y+1))
+    z_list = list(range(z+1))
+    perm_list = [[x,y,z] for x in x_list for y in y_list for z in z_list]
+    list_without_sums = [list for list in perm_list if sum(list) != 2]
+    print(list_without_sums)
