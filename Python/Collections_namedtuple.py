e5eeb1ecffa2defd07f89695a2919fee7179a970
Ilya <htctomsk634@gmail.com>
2022-11-15 17:30:55 +0700

    Create Collections_namedtuple.py

---
+++ Python/Collections_namedtuple.py
@@ -0,0 +1,15 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import namedtuple
+
+n = int(input())
+fields = input()
+marks_sum = 0
+
+Students = namedtuple('Students', fields)
+
+for i in range(n):
+    student = Students(*input().split())
+    marks_sum += int(student.MARKS)
+    
+avg = marks_sum/n
+print(round(avg,2))
